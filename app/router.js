ea426f4b9ea1836dfbbfb0b8da4440b48fc300e1
Arlen Burton and Andrew Russo <arlen@>
2016-06-15 13:43:42 -0700

    Completed dynamic route of the products to display info on individual pages.

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.route('product');
+  this.route('product', {path: '/product/:product_id'});
   this.route('checkout');
 });
 
